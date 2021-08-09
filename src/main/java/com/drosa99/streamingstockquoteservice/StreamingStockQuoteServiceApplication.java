493cd5be03225ad50e12eeef2c3b6924d3728ef9
Daniela Amaral <danielarosa2309@gmail.com>
2021-08-09 19:49:24 -0300

    Initial commit

---
+++ src/main/java/com/drosa99/streamingstockquoteservice/StreamingStockQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.drosa99.streamingstockquoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingStockQuoteServiceApplication {
+
+		public static void main(String[] args) {
+				SpringApplication.run(StreamingStockQuoteServiceApplication.class, args);
+		}
+
+}
---
+++ src/test/java/com/drosa99/streamingstockquoteservice/StreamingStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drosa99.streamingstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingStockQuoteServiceApplicationTests {
+
+		@Test
+		void contextLoads() {
+		}
+
+}
